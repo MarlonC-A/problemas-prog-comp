30ce1fecd6289950e36d676e32ec5d8ce93076b5
Marlon <marlon62009@gmail.com>
2020-02-01 13:30:27 -0600

    Agregar 'ElCódigoGray'

---
+++ ClubUPIIZ/Problemas2020-2/Bits/ElCódigoGray.cpp
@@ -0,0 +1,15 @@
+// https://omegaup.com/arena/problem/gray#problems
+// MarlonC-A, 2020
+
+#include<iostream>
+using namespace std;
+unsigned long n,m;
+
+int main(){
+    
+    cin>>n;
+    m=n>>1;
+    cout<<(n^m)<<endl;
+
+return 0;
+}
