4d7fe8e3d7fc8b648ba7e96e5eb5199a0723ebf9
Marlon <marlon62009@gmail.com>
2020-02-09 21:55:22 -0600

    Agregar template de C++

---
+++ templateCpp.cpp
@@ -0,0 +1,24 @@
+// MarlonC-A, 2020
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define w(x) cout<<"'"<<(#x)<<"' es "<<x<<endl;
+#define EPS 1.19209e-07
+#define fastIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+typedef long long int lol;
+typedef pair<int,int> pii;
+lol BIG = 1e9+7;
+
+int64_t bigcin(){ //Método de entrada para enteros de 64 bits con signo, usar como "int64_t [var] = bigcin();"
+    string cad;
+    cin>>cad;
+    int64_t numerote = stoll(cad);
+    return numerote;
+}
+
+int main(){
+    fastIO
+    
+return 0;
+}
