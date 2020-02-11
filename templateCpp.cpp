b17af4252e429eee5ddcd2ed3feaf9d484afb3f1
Marlon <marlon62009@gmail.com>
2020-02-10 19:55:30 -0600

    Corregir método de entrada para __int128

--- templateCpp.cpp
+++ templateCpp.cpp
@@ -4,17 +4,17 @@
 using namespace std;
 
 #define w(x) cout<<"'"<<(#x)<<"' es "<<x<<endl;
+#define bIn(x) bigcin(&x);
 #define EPS 1.19209e-07
 #define fastIO ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 typedef long long int lol;
 typedef pair<int,int> pii;
 lol BIG = 1e9+7;
 
-int64_t bigcin(){ //Método de entrada para enteros de 64 bits con signo, usar como "int64_t [var] = bigcin();"
+void bigcin(__int128 *numerote){ //Método de entrada para enteros de 128 bits con signo, usar como "bIn([var]);"
     string cad;
     cin>>cad;
-    int64_t numerote = stoll(cad);
-    return numerote;
+    *numerote = stoll(cad);
 }
 
 int main(){
